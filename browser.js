6e0f007eb8c5682ad09cc683bdeae2c45dab1850
sit79 <stefan.tolksdorf@gmail.com>
2021-04-11 18:15:02 +0200

    Removed console.log()s
    used console.error() where appropriate

--- browser.js
+++ browser.js
@@ -5,14 +5,14 @@ async function startBrowser() {
   let browser, spinner;
   try {
     spinner = ora("opening the browser").start();
-    // console.log("opening the browser");
+    // opening the browser
     browser = await puppeteer.launch({
       headless: true,
       args: ["--disable-setuid-sandbox"],
       ignoreHTTPSErrors: true,
     });
   } catch (error) {
-    console.log("Could not create a browser instance => : ", error);
+    console.error("Could not create a browser instance => : ", error);
   }
   spinner.succeed();
   return browser;
