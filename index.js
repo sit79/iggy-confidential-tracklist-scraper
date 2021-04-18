adda6611d7ea6ba3f239f7f876347dd53c852c46
sit79 <stefan.tolksdorf@gmail.com>
2021-04-18 13:39:37 +0200

    no cron

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-const CronJob = require("cron").CronJob;
 const browserObject = require("./browser");
 const scraperController = require("./pageController");
 
-const job = new CronJob({
-  cronTime: process.env.CRONTIME,
-  onTick: async function () {
-    console.clear();
-    // start the browser and create a browser instance
-    let browserInstance = await browserObject.startBrowser();
-    // pass the browser instance to the scraper controller
-    await scraperController(browserInstance);
-  },
-  start: true,
-  runOnInit: false,
-});
-
-job.start();
+console.clear();
+// start the browser and create a browser instance
+let browserInstance = browserObject.startBrowser();
+// pass the browser instance to the scraper controller
+scraperController(browserInstance);
