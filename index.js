f1fdaebe2d75123f7b41305255107da51d9737ff
Stefan Tolksdorf <stefan.tolksdorf@cgi.com>
2022-04-03 19:08:38 +0200

    Removed console.clear()

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 const browserObject = require("./browser");
 const scraperController = require("./pageController");
 
-console.clear();
 // start the browser and create a browser instance
 let browserInstance = browserObject.startBrowser();
 // pass the browser instance to the scraper controller
