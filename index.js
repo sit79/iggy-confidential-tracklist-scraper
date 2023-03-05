149c0469bfe25542a9dd16cd1007f95330b51f50
Stefan Tolksdorf <stefan.tolksdorf@icloud.com>
2023-03-05 11:13:30 +0100

    Test

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
-import { browserObject } from "./browser.js";
-import { scraperController } from "./pageController.js";
+// import { browserObject } from "./browser.js";
+// import { scraperController } from "./pageController.js";
+
+console.log('HALLO');
 
 // start the browser and create a browser instance
-let browserInstance = browserObject.startBrowser();
+// let browserInstance = browserObject.startBrowser();
 // pass the browser instance to the scraper controller
-scraperController(browserInstance);
+// scraperController(browserInstance);
