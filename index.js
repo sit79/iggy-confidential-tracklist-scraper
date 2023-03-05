a38c547dd0c008f78d11e73d3264e59221db3ec5
Stefan Tolksdorf <stefan.tolksdorf@icloud.com>
2023-03-05 10:52:12 +0100

    Test

--- index.js
+++ index.js
@@ -2,9 +2,7 @@
 import { browserObject } from "./browser.js";
 import { scraperController } from "./pageController.js";
 
-import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config();
-console.log(process.env)
+console.log('RUNNING');
 
 // start the browser and create a browser instance
 // let browserInstance = browserObject.startBrowser();
