0196ce19ac74cd1d276e603145d1c1549fcda8fb
Stefan Tolksdorf <stefan.tolksdorf@icloud.com>
2023-03-05 10:47:06 +0100

    Another try

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ import { scraperController } from "./pageController.js";
 
 import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config();
+console.log(process.env)
 
 // start the browser and create a browser instance
-let browserInstance = browserObject.startBrowser();
+// let browserInstance = browserObject.startBrowser();
 // pass the browser instance to the scraper controller
-scraperController(browserInstance);
+// scraperController(browserInstance);
